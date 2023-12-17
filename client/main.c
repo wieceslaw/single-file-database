c00a71bdb7c86afde75b42cf82ce38a320c86939
Wyacheslav <vyache-nov@mail.ru>
2023-12-17 18:17:26 +0300

    added client

---
+++ client/main.c
@@ -0,0 +1,13 @@
+//
+// Created by vyach on 19.11.2023.
+//
+
+#include <stdio.h>
+#include "example.pb-c.h"
+
+int main() {
+    PhoneNumber phoneNumber = PHONE_NUMBER__INIT;
+    phoneNumber.
+    printf("Hello, world \n");
+    return 0;
+}
